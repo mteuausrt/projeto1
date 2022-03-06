3ed6fb6fd9734ba9077fa90a473d7e400cb6e637
muriloscf <furychicago@gmail.com>
2022-03-06 13:10:51 -0300

    alterando estilo

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
 //esse script foi criado pelo site do github
 //com o comando pull, foi adicionado ao nosso repositorio local
+
+const btn = document.querySelectorAll('button')[0];
+
+btn.addEventListener('click', () => {
+
+    if(btn.classList.contains('clicked')){
+        btn.classList.remove('clicked')
+        btn.classList.add('disclicked')
+    }else{
+        btn.classList.add('clicked')
+        btn.classList.remove('disclicked')
+    }
+
+})
